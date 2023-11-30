920eb788a94b257b0dfade4133b553b0f470be8a
zeteoh <teohzeloong12@gmail.com>
2023-11-30 23:10:57 +0800

    added health bar

--- src/scenes/battle-scene.js
+++ src/scenes/battle-scene.js
@@ -1,4 +1,9 @@
-import { BATTLE_BACKGROUND_ASSET_KEYS, MONSTER_ASSET_KEYS } from "../assets/asset-key.js";
+import {
+  BATTLE_ASSET_KEYS,
+  BATTLE_BACKGROUND_ASSET_KEYS,
+  HEALTH_BAR_ASSET_KEYS,
+  MONSTER_ASSET_KEYS,
+} from "../assets/asset-key.js";
 import Phaser from "../lib/phaser.js";
 import { SCENE_KEYS } from "./scene-keys.js";
 
@@ -25,9 +30,115 @@ export class BattleScene extends Phaser.Scene {
      * @setOrigin - setting 0 means that the object is placed in the middle. TLDR, it holds the middle point of the image
      */
     this.add.image(0, 0, BATTLE_BACKGROUND_ASSET_KEYS.FOREST).setOrigin(0);
-    console.log(`[${BattleScene.name}:create] invoked`)
-    this.add.image(768, 144, MONSTER_ASSET_KEYS.CARNODUSK, 0)
-    this.add.image(256, 316, MONSTER_ASSET_KEYS.IGUANIGNITE, 0).setFlipX(true)
+    console.log(`[${BattleScene.name}:create] invoked`);
+    this.add.image(768, 144, MONSTER_ASSET_KEYS.CARNODUSK, 0);
+    this.add.image(256, 316, MONSTER_ASSET_KEYS.IGUANIGNITE, 0).setFlipX(true);
 
+    /**
+     * using a container, we can move all assets in the container together and package them in a container
+     */
+    //render out the player health bar
+    const playerMonsterName = this.add.text(
+      30,
+      20,
+      MONSTER_ASSET_KEYS.IGUANIGNITE,
+      {
+        color: "#7E3D3F",
+        fontSize: "32px",
+      }
+    );
+    this.add.container(556, 318, [
+      this.add
+        .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
+        .setOrigin(0),
+      playerMonsterName,
+      this.#createHealth(34, 34),
+      this.add.text(
+        playerMonsterName.width + 35,
+        23,
+        'L5',
+        {
+          color: "#ED474B",
+          fontSize: "28px",
+        }
+      ),
+      this.add.text(
+        30,
+        55,
+        'HP',
+        {
+          color: "#FF6505",
+          fontSize: "24px",
+          fontStyle: 'italic'
+        }
+      ),
+      this.add.text(
+        443,
+        80,
+        '25/25',
+        {
+          color: "#7E3D3F",
+          fontSize: "16px"
+        }
+        //added set origin 1,0 so that it will always align with the health bar
+      ).setOrigin(1,0)
+    ]);
+
+    //render out the enemy health bar
+    const enemyMonsterName = this.add.text(
+        30,
+        20,
+        MONSTER_ASSET_KEYS.CARNODUSK,
+        {
+          color: "#7E3D3F",
+          fontSize: "32px",
+        }
+      );
+      this.add.container(0, 0, [
+        this.add
+          .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
+          .setOrigin(0).setScale(1,0.8),
+          enemyMonsterName,
+        this.#createHealth(34, 34),
+        this.add.text(
+            enemyMonsterName.width + 35,
+          23,
+          'L5',
+          {
+            color: "#ED474B",
+            fontSize: "28px",
+          }
+        ),
+        this.add.text(
+          30,
+          55,
+          'HP',
+          {
+            color: "#FF6505",
+            fontSize: "24px",
+            fontStyle: 'italic'
+          }
+        ),
+      ]);
+  }
+
+  #createHealth(x, y) {
+    const scaleY = 0.7;
+    const leftCap = this.add
+      .image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP)
+      .setOrigin(0, 0.5)
+      .setScale(1, scaleY);
+    const middle = this.add
+      .image(leftCap.x + leftCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
+      .setOrigin(0, 0.5)
+      .setScale(1, scaleY);
+    //stretches the health bar
+    middle.displayWidth = 360;
+    const rightCap = this.add
+      .image(middle.x + middle.displayWidth, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP)
+      .setOrigin(0, 0.5)
+      .setScale(1, scaleY);
+
+    return this.add.container(x, y, [leftCap, middle, rightCap]);
   }
 }
