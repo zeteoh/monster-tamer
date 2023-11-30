26bc90c2d6208438f5120aa457c10138d91391e1
zeteoh <teohzeloong12@gmail.com>
2023-12-01 01:16:33 +0800

    added attack menu selection

--- src/scenes/battle-scene.js
+++ src/scenes/battle-scene.js
@@ -7,6 +7,17 @@ import {
 import Phaser from "../lib/phaser.js";
 import { SCENE_KEYS } from "./scene-keys.js";
 
+const BATTLE_MENU_OPTIONS = Object.freeze({
+    FIGHT: 'FIGHT',
+    SWITCH: 'SWTICH',
+    ITEM: 'ITEM',
+    FLEE: 'FLEE'
+})
+
+const battleUiTextStyle = {
+    color: "black",
+    fontSize: "30px",
+}
 export class BattleScene extends Phaser.Scene {
   constructor() {
     super({
@@ -52,77 +63,77 @@ export class BattleScene extends Phaser.Scene {
         .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
         .setOrigin(0),
       playerMonsterName,
-      this.#createHealth(34, 34),
-      this.add.text(
-        playerMonsterName.width + 35,
-        23,
-        'L5',
-        {
-          color: "#ED474B",
-          fontSize: "28px",
-        }
-      ),
-      this.add.text(
-        30,
-        55,
-        'HP',
-        {
-          color: "#FF6505",
-          fontSize: "24px",
-          fontStyle: 'italic'
-        }
-      ),
-      this.add.text(
-        443,
-        80,
-        '25/25',
-        {
-          color: "#7E3D3F",
-          fontSize: "16px"
-        }
-        //added set origin 1,0 so that it will always align with the health bar
-      ).setOrigin(1,0)
+      this.#createHealthBar(34, 34),
+      this.add.text(playerMonsterName.width + 35, 23, "L5", {
+        color: "#ED474B",
+        fontSize: "28px",
+      }),
+      this.add.text(30, 55, "HP", {
+        color: "#FF6505",
+        fontSize: "24px",
+        fontStyle: "italic",
+      }),
+      this.add
+        .text(
+          443,
+          80,
+          "25/25",
+          {
+            color: "#7E3D3F",
+            fontSize: "16px",
+          }
+          //added set origin 1,0 so that it will always align with the health bar
+        )
+        .setOrigin(1, 0),
     ]);
 
     //render out the enemy health bar
     const enemyMonsterName = this.add.text(
-        30,
-        20,
-        MONSTER_ASSET_KEYS.CARNODUSK,
-        {
-          color: "#7E3D3F",
-          fontSize: "32px",
-        }
-      );
-      this.add.container(0, 0, [
-        this.add
-          .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
-          .setOrigin(0).setScale(1,0.8),
-          enemyMonsterName,
-        this.#createHealth(34, 34),
-        this.add.text(
-            enemyMonsterName.width + 35,
-          23,
-          'L5',
-          {
-            color: "#ED474B",
-            fontSize: "28px",
-          }
-        ),
-        this.add.text(
-          30,
-          55,
-          'HP',
-          {
-            color: "#FF6505",
-            fontSize: "24px",
-            fontStyle: 'italic'
-          }
-        ),
-      ]);
+      30,
+      20,
+      MONSTER_ASSET_KEYS.CARNODUSK,
+      {
+        color: "#7E3D3F",
+        fontSize: "32px",
+      }
+    );
+    this.add.container(0, 0, [
+      this.add
+        .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
+        .setOrigin(0)
+        .setScale(1, 0.8),
+      enemyMonsterName,
+      this.#createHealthBar(34, 34),
+      this.add.text(enemyMonsterName.width + 35, 23, "L5", {
+        color: "#ED474B",
+        fontSize: "28px",
+      }),
+      this.add.text(30, 55, "HP", {
+        color: "#FF6505",
+        fontSize: "24px",
+        fontStyle: "italic",
+      }),
+    ]);
+    //render out the main info and sub info pane
+    this.#createMainInfoPane();
+    this.add.container(520, 448, [
+        this.#createMainInfoSubPane(),
+        this.add.text(50,22, BATTLE_MENU_OPTIONS.FIGHT,battleUiTextStyle),
+        this.add.text(240,22, BATTLE_MENU_OPTIONS.SWITCH,battleUiTextStyle),
+        this.add.text(50,70, BATTLE_MENU_OPTIONS.ITEM,battleUiTextStyle),
+        this.add.text(240,70, BATTLE_MENU_OPTIONS.FLEE,battleUiTextStyle)
+    ])
+
+    this.add.container(0,449,[
+        this.add.text(55,22, 'slash',battleUiTextStyle),
+        this.add.text(240,22, 'growl',battleUiTextStyle),
+        this.add.text(55,70, '-',battleUiTextStyle),
+        this.add.text(240,70, '-',battleUiTextStyle),
+
+    ])
   }
 
-  #createHealth(x, y) {
+  #createHealthBar(x, y) {
     const scaleY = 0.7;
     const leftCap = this.add
       .image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP)
@@ -140,5 +151,38 @@ export class BattleScene extends Phaser.Scene {
       .setScale(1, scaleY);
 
     return this.add.container(x, y, [leftCap, middle, rightCap]);
+  }
+
+  #createMainInfoPane() {
+    const padding = 4;
+    const rectHeight = 124;
+    //last argument is alpha (transparency value)
+    this.add
+      .rectangle(
+        padding,
+        this.scale.height - rectHeight - padding,
+        this.scale.width - padding * 2,
+        rectHeight,
+        0xede4f3,
+        1
+      )
+      .setOrigin(0)
+      .setStrokeStyle(8, 0xe4434a, 1);
+  }
+
+  #createMainInfoSubPane() {
+    const rectWidth = 500;
+    const rectHeight = 124;
+    //last argument is alpha (transparency value)
+    return this.add
+      .rectangle(
+        0,0,
+        rectWidth,
+        rectHeight,
+        0xede4f3,
+        1
+      )
+      .setOrigin(0)
+      .setStrokeStyle(8, 0x905ac2, 1);
   }
 }
